be8964902f658427d2c800aec5da813e58dd4c26
JDRproject <jdrproject7949@gmail.com>
2022-02-20 17:16:20 +0900

    setting

--- twitproject/settings.py
+++ twitproject/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'twitproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'db.mysql',
     }
 }
 
@@ -105,11 +105,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
