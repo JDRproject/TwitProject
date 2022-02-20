3e0bb7c52f22c472970d09ac1a46e0da7a15b945
JDRproject <jdrproject7949@gmail.com>
2022-02-20 19:12:55 +0900

    1

--- twitproject/settings.py
+++ twitproject/settings.py
@@ -83,9 +83,6 @@ DATABASES = {
         'PASSWORD' : 'db',
         'HOST' : 'localhost',
         'PORT' : '3306',
-        'OPTIONS': {
-            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
-        }
     }
 }
 
