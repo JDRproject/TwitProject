d85bdc6f9214222bf98027e290298c444088b60d
JDRproject <jdrproject7949@gmail.com>
2022-02-20 20:47:08 +0900

    1

--- collect/models.py
+++ collect/models.py
@@ -5,5 +5,5 @@ class Twits(models.Model):
     user_id = models.CharField(max_length=50)
     text = models.TextField()
     
-    def __str__(self):
-        return str(self.id ,self.user_id, self.text)
+    #def __str__(self):
+    #    return str(self.id ,self.user_id, self.text)
