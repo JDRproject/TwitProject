aac69cff7d1d38fd3fc59224a38e92375acbdcbb
JDRproject <jdrproject7949@gmail.com>
2022-02-21 10:16:02 +0900

    1

--- collect/models.py
+++ collect/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Tweet(models.Model):
+
     name = models.CharField(max_length=50)
     text = models.TextField()
     time = models.DateField()
